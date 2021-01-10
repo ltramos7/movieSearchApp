045cba37a1b4ec4636cad154ceee1c6df71a4471
Linda Ramos <ltramos7@gmail.com>
2021-01-10 15:00:38 -0600

    feat: search bar and button added via SearchArea component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import HeaderSection from './components/HeaderSection/HeaderSection'
+import SearchArea from './components/SearchArea/SearchArea'
 
 function App() {
   return (
     <div className="App">
       <HeaderSection/>
+      <SearchArea/>
     </div>
   );
 }
---
+++ src/components/SearchArea/SearchArea.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class extends Component {
+    render() {
+        return (
+            <div>
+                <section>
+                    <form>
+                        <input placeholder="Search Here..."></input>
+                        <button>Search</button>
+                    </form>
+                </section>
+            </div>
+        )
+    }
+}
