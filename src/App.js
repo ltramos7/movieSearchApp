26541e84f684248f2b6067238d7f176d650fd33e
Linda Ramos <ltramos7@gmail.com>
2021-01-10 16:26:20 -0600

    feat:  set search value in app state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import React, { Component } from 'react'
+
 import './App.css';
 import HeaderSection from './components/HeaderSection/HeaderSection'
 import SearchArea from './components/SearchArea/SearchArea'
 
-function App() {
-  return (
-    <div className="App">
-      <HeaderSection/>
-      <SearchArea/>
-    </div>
-  );
+
+export default class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      searchedMovie: ""
+    }
+  }
+
+  setSearchedValue = (e) => {
+    e.preventDefault()
+    
+    this.setState({
+      searchedMovie: e.target.value
+    })
+  }
+
+  render() {
+    console.log("searched Movie: ", this.state.searchedMovie)
+    return (
+      <div className="App">
+        <HeaderSection/>
+        <SearchArea setSearchedValue={this.setSearchedValue}/>
+      </div>  
+    )
+  }
 }
 
-export default App;
--- src/components/SearchArea/SearchArea.js
+++ src/components/SearchArea/SearchArea.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 
-export default class extends Component {
+export default class SearchArea extends Component {
+
+   
+
     render() {
         return (
             <div>
                 <section>
                     <form>
-                        <input placeholder="Search Here..."></input>
+                        <input placeholder="Search Here..." onChange={this.props.setSearchedValue}></input>
                         <button>Search</button>
                     </form>
                 </section>
