315ae380c837ae0a369d4faad923b4123a6ce0c3
Linda Ramos <ltramos7@gmail.com>
2021-01-10 18:12:15 -0600

    feat: disable nominate button after click

--- src/components/MatchingMovieCard/MatchingMovieCard.js
+++ src/components/MatchingMovieCard/MatchingMovieCard.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 
 export default class MatchingMovieCard extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            disabled: false
+        }
+    }
+
+    disableNominateBtn = () => {
+        this.setState({
+            disabled: true
+        })
+    }
+
     render() {
         return (
             <div>
                 <p>Title: {this.props.title}</p>
                 <p>Release Year: {this.props.year}</p>
-                <button>Nominate</button>
+                <button disabled={this.state.disabled} onClick={this.disableNominateBtn}>Nominate</button>
             </div>
         )
     }
