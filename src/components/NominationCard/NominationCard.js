e8adca50ccb7cbe6c9ca5ebfc41d4befff411c67
Linda Ramos <ltramos7@gmail.com>
2021-01-13 14:08:31 -0600

    feat: NominationCard css added

--- src/components/NominationCard/NominationCard.js
+++ src/components/NominationCard/NominationCard.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import './NominationCard.css'
 
 export default class NominationCard extends Component {
     render() {
         console.log(this.props.disabled)
         let movie = this.props.movie
         return (
-            <div>
+            <div className="nominationCard">
                 <p>Title: {movie.Title}</p>
                 <p>Year: {movie.Year}</p>
                 <button onClick={()=>{this.props.removeNominatedMoive(movie); this.props.toggleDisabledValue();}}>Remove</button>
