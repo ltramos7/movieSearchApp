37fa35492a7c099a08cd5108e9b15860fda24aff
Linda Ramos <ltramos7@gmail.com>
2021-01-17 15:04:11 -0600

    refactor: removed unused login signup buttons

--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -9,14 +9,7 @@ export default class HeaderSection extends Component {
                     <div className="title">
                         <h1>The Shoppies</h1>
                         <p>Like the Dundees, but for movies.</p>
-                    </div>
-                    <div className="navButtons">
-                        <form>
-                            <button>Login</button>
-                            <button>Signup</button>
-                        </form>
-                    </div>
-                
+                    </div>                
             </div>
         )
     }
